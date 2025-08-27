76e7c41a5439b79dcbaa7fc88b8042b2839f96c6
mooujarrar <mohyiddine1997@gmail.com>
2025-08-27 19:14:25 +0200

    add ImageData and calculate center of the image and all the potential radiuses

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use show_image::{event, Image, create_window};
 
 
 // This is the length of the rotating strip fixed into the wheels radius
-const LED_STRIP_LENGTH: u32 = 14;
+const LED_STRIP_LENGTH: u8 = 14; // This are the image dimensions to be displayed on the wheel
+const IMAGE_WIDTH: u8 = LED_STRIP_LENGTH*2;
+const IMAGE_HEIGHT: u8 = IMAGE_WIDTH;
 
 /*
  * This reads the image into a DynamicImage.
@@ -21,7 +23,7 @@ fn read_image() -> DynamicImage {
         Err(error) => panic!("Problem opening file, reason {error}"),
     };
     // If everything is fine and the image is read, we try to resize it
-    decoded_content.resize_exact(LED_STRIP_LENGTH*2, LED_STRIP_LENGTH*2, FilterType::Triangle)
+    decoded_content.resize_exact(u32::from(IMAGE_WIDTH), u32::from(IMAGE_HEIGHT), FilterType::Triangle)
 }
 
 // Take a DynamicImage and convert it to RgbImage (Clone and moving ownership)
@@ -47,11 +49,51 @@ fn display_image(image: RgbImage) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[show_image::main]
+/*
+ * This function computes for a given angle of the strip the full RGB strip values to show
+ */
+fn polar_to_cartesian(image: &RgbImage, data: &ImageData, theta: f32) {
+    // Use both the image and the processed center data etc to start getting pixels
+}
+
+struct ImageData {
+    middle_x: f32,
+    middle_y: f32,
+    r_max: f32,
+    radius_vector: Vec<f32>,
+}
+impl ImageData {
+    pub fn new() -> ImageData {
+        // Create a virtual center in case the strip dimensions are Even  number (there is no real
+        // median or middle point)
+        let (middle_x, middle_y, r_max) = (
+            (f32::from(IMAGE_WIDTH) - 1f32) / 2.0,
+            (f32::from(IMAGE_HEIGHT) - 1f32) / 2.0,
+            (f32::from(LED_STRIP_LENGTH) * 2.0 - 1f32) / 2.0
+        );
+        println!("Create new ImageDatan, center of the picture: x={middle_x}, y={middle_y}");
+        let mut r: Vec<f32> = vec![0.0;LED_STRIP_LENGTH as usize];
+        for i in 0..LED_STRIP_LENGTH {
+            r[i as usize] = f32::from(i) * r_max / (f32::from(LED_STRIP_LENGTH)-1f32);
+        }
+        println!("Radius values are: {:?}", r);
+        ImageData {
+            middle_x: middle_x,
+            middle_y: middle_y,
+            r_max: r_max,
+            radius_vector: r,
+        }
+    }
+}
+
+// #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img_content = read_image();
     let rgb_image = convert_to_rgb8(&img_content);
     // Check the dimensions of the output pic
     println!("Dimensions of the scaled pic are: Width: {}, Height: {}", rgb_image.width(), rgb_image.height());
-    display_image(rgb_image.clone())
+    // display_image(rgb_image.clone())
+    let image_data = ImageData::new();
+    polar_to_cartesian(&rgb_image, &image_data, 1.2);
+    Ok(())
 }
