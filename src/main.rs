61912cd942ece79e95d2f613019ac5b07ad319db
mooujarrar <mohyiddine1997@gmail.com>
2025-09-05 16:02:27 +0200

    maint; disable showing the image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod image_processor;
 use image_processor::{ImageData, N_SLICES};
 
-#[show_image::main]
+// #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img_content = image_processor::read_image();
     let rgb_image = image_processor::convert_to_rgb8(&img_content);
@@ -10,6 +10,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let image_data = ImageData::new();
     let colors_vector = image_processor::prepare_color_vector(&rgb_image, &image_data);
     println!("The vector of colors for an angle step: {} is {:#?}", N_SLICES, colors_vector);
-    // Ok(())
-    image_processor::display_image(rgb_image.clone())
+    Ok(())
+    // image_processor::display_image(rgb_image.clone())
 }
